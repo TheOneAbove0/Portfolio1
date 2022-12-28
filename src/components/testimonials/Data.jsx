59cc7d37a1e397530ac6115533a7c7687e01576d
Manish Thapaliya <manishthapaliyaofficial@gmail.com>
2022-12-28 13:23:12 +0545

    first commit

---
+++ src/components/testimonials/Data.jsx
@@ -0,0 +1,28 @@
+import Image1 from '../assets/Image1.jpg';
+import Image2 from '../assets/Image2.jpg';
+import Image3 from '../assets/Image3.jpg';
+
+export const Data = [
+    {
+      id: 1,
+      image: Image1,
+      title: "Parshant Ale",
+      description:
+        "A really good job, all aspects of the project were followed step by step and with good results.",
+    },
+    {
+      id: 2,
+      image: Image2,
+      title: "Sagar shrestha",
+      description:
+        "A really good job, all aspects of the project were followed step by step and with good results.",
+    },
+    {
+      id: 3,
+      image: Image3,
+      title: "Pradeep Karki",
+      description:
+        "A really good job, all aspects of the project were followed step by step and with good results.",
+    },
+  ];
+
